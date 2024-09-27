236e5e361b3754a87baacb772fe6963a769115cd
Tito0ficial <titol.oficial@gmail.com>
2024-09-27 15:32:55 -0300

    Estruturando camadas e método GET

---
+++ src/main/java/com/devsuperior/desafiocrud/services/ClientService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.desafiocrud.services;
+
+import com.devsuperior.desafiocrud.dto.ClientDTO;
+import com.devsuperior.desafiocrud.entities.Client;
+import com.devsuperior.desafiocrud.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client =  repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+}
