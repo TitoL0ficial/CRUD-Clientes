362ef7e1343adda7f1a2fe811200397ab42dd721
Tito0ficial <titol.oficial@gmail.com>
2024-09-28 17:57:34 -0300

    Tratamento de exceção findById

--- src/main/java/com/devsuperior/desafiocrud/services/ClientService.java
+++ src/main/java/com/devsuperior/desafiocrud/services/ClientService.java
@@ -3,6 +3,7 @@ package com.devsuperior.desafiocrud.services;
 import com.devsuperior.desafiocrud.dto.ClientDTO;
 import com.devsuperior.desafiocrud.entities.Client;
 import com.devsuperior.desafiocrud.repositories.ClientRepository;
+import com.devsuperior.desafiocrud.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,7 +18,8 @@ public class ClientService {
 
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
-        Client client =  repository.findById(id).get();
+        Client client =  repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ClientDTO(client);
     }
 
